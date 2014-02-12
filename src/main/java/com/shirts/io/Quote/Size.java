7dcbb2b3f7e63086d0266e6566677f21082ee0e9
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-02-11 19:44:19 -0500

    A list of sizes was added. Functions were also added to make it easier to get sizes

--- src/main/java/com/shirts/io/Quote/Size.java
+++ src/main/java/com/shirts/io/Quote/Size.java
@@ -1,5 +1,9 @@
 package com.shirts.io.Quote;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Size
 {
     private int xxsml;
@@ -13,97 +17,185 @@ public class Size
     private int xxxxl;
     private int xxxxxl;
     private int xxxxxxl;
+    public static List<String> sizes = Arrays.asList("xxsml","xsml",
+                                               "sml","med",
+                                               "lrg","xlg",
+                                               "xxl","xxxl",
+                                               "xxxxl","xxxxxl","xxxxxxl");
+    HashMap<String,Integer> actualSizes = new HashMap<String, Integer>();
 
     public Size()
     {
         super();
+        actualSizes.put("xxsml",0);
+        actualSizes.put("xsml",0);
+        actualSizes.put("sml",0);
+        actualSizes.put("med",0);
+        actualSizes.put("lrg",0);
+        actualSizes.put("xlg",0);
+        actualSizes.put("xxl",0);
+        actualSizes.put("xxxl",0);
+        actualSizes.put("xxxxl",0);
+        actualSizes.put("xxxxxl",0);
+        actualSizes.put("xxxxxxl",0);
+    }
+
+    public int getSize(String size)
+    {
+        return actualSizes.get(size);
     }
 
     public int getXxsml() {
-        return xxsml;
+        return actualSizes.get("xxsml");
     }
 
     public void setXxsml(int xxsml) {
         this.xxsml = xxsml;
+        actualSizes.put("xxsml",xxsml);
     }
 
     public int getXsml() {
-        return xsml;
+        return actualSizes.get("xsml");
     }
 
     public void setXsml(int xsml) {
         this.xsml = xsml;
+        actualSizes.put("xsml",xsml);
     }
 
     public int getSml() {
-        return sml;
+        return actualSizes.get("sml");
     }
 
     public void setSml(int sml) {
         this.sml = sml;
+        actualSizes.put("sml",sml);
     }
 
     public int getMed() {
-        return med;
+        return actualSizes.get("med");
     }
 
     public void setMed(int med) {
         this.med = med;
+        actualSizes.put("med",med);
     }
 
     public int getLrg() {
-        return lrg;
+        return actualSizes.get("lrg");
     }
 
     public void setLrg(int lrg) {
         this.lrg = lrg;
+        actualSizes.put("lrg",lrg);
     }
 
     public int getXlg() {
-        return xlg;
+        return actualSizes.get("xlg");
     }
 
     public void setXlg(int xlg) {
         this.xlg = xlg;
+        actualSizes.put("xlg",xlg);
     }
 
     public int getXxl() {
-        return xxl;
+        return actualSizes.get("xxl");
     }
 
     public void setXxl(int xxl) {
         this.xxl = xxl;
+        actualSizes.put("xxl",xxl);
     }
 
     public int getXxxl() {
-        return xxxl;
+        return actualSizes.get("xxxl");
     }
 
     public void setXxxl(int xxxl) {
         this.xxxl = xxxl;
+        actualSizes.put("xxxl",xxxl);
     }
 
     public int getXxxxl() {
-        return xxxxl;
+        return actualSizes.get("xxxxl");
     }
 
     public void setXxxxl(int xxxxl) {
         this.xxxxl = xxxxl;
+        actualSizes.put("xxxxl",xxxxl);
     }
 
     public int getXxxxxl() {
-        return xxxxxl;
+        return actualSizes.get("xxxxxl");
     }
 
     public void setXxxxxl(int xxxxxl) {
         this.xxxxxl = xxxxxl;
+        actualSizes.put("xxxxxl",xxxxxl);
     }
 
     public int getXxxxxxl() {
-        return xxxxxxl;
+        return actualSizes.get("xxxxxxl");
     }
 
     public void setXxxxxxl(int xxxxxxl) {
         this.xxxxxxl = xxxxxxl;
+        actualSizes.put("xxxxxxl",xxxxxxl);
+    }
+
+    public static String xxsml()
+    {
+        return "xxsml";
+    }
+
+    public static String xsml()
+    {
+        return "xsml";
+    }
+
+    public static String sml()
+    {
+        return "sml";
+    }
+
+    public static String med()
+    {
+        return "med";
+    }
+
+    public static String lrg()
+    {
+        return "lrg";
+    }
+
+    public static String xlg()
+    {
+        return "xlg";
+    }
+
+    public static String xxlg()
+    {
+        return "xxlg";
+    }
+
+    public static String xxxlg()
+    {
+        return "xxxlg";
+    }
+
+    public static String xxxxlg()
+    {
+        return "xxxxlg";
+    }
+
+    public static String xxxxxlg()
+    {
+        return "xxxxxlg";
+    }
+
+    public static String xxxxxxlg()
+    {
+        return "xxxxxxlg";
     }
 }
